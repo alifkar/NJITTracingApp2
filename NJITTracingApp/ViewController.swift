7fda772107abd6b5e0bba1dafd6f802ffdd695d0
Angie Pinilla <angelinepinilla@gmail.com>
2016-03-11 13:10:41 -0500

    hello

--- NJITTracingApp/ViewController.swift
+++ NJITTracingApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let x = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
